c5568e4e86057ea05b604ace68079427da172200
Mel Baylon <melbaylon@3210m.local>
2026-03-05 15:06:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	sortByTime := flag.Bool("t", false, "sort by modification time")
 	sortBySize := flag.Bool("S", false, "sort by file size") // Note: original ls uses -S for size
 	reverse := flag.Bool("r", false, "reverse order while sorting")
+	classify := flag.Bool("F", false, "append indicator (one of */=>@|) to entries")
 
 	flag.Parse()
 
@@ -26,9 +27,9 @@ func main() {
 
 	for _, path := range paths {
 		// Basic orchestration for Milestone 1
-		err := ls.List(path, *all, *long, *sortByTime, *sortBySize, *reverse)
+		err := ls.List(path, *all, *long, *sortByTime, *sortBySize, *reverse, *classify)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "ls: %s: %v\\n", path, err)
+			fmt.Fprintf(os.Stderr, "ls: %s: %v\n", path, err)
 		}
 	}
 }
